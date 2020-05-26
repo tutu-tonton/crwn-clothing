1874aabbb5d8aeabb7a4f0459ef9b023f94dd49c
tutu-tonton <tngt33844507@gmail.com>
2020-05-26 14:39:11 +0900

    update our meu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-	// 分割代入で書く
-	// const MenuItem = (props) => (
-	<div className={`${size} menu-item`}>
+// 分割代入で書く
+// const MenuItem = (props) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+	<div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
 		<div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
 		<div className="content">
 			<h1 className="title">{title.toUpperCase()}</h1>
@@ -14,4 +15,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
 	</div>
 );
 
-export default MenuItem;
+// withRouterで囲むことで、historyが使える
+export default withRouter(MenuItem);
