97e9f2cb920a8b5ffef007e179433a9156a0be9e
tutu-tonton <tngt33844507@gmail.com>
2020-05-31 13:03:33 +0900

    added cartIcon an cartDropdown components to our app, also implemented cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -8,7 +8,9 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
 	user: userReducer,
+	cart: cartReducer,
 });
