515a877ad8b94be5714254374c49c3c18889eacd
tutu-tonton <tngt33844507@gmail.com>
2020-05-31 18:03:28 +0900

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -4,6 +4,7 @@
 //========================================
 
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
 	hidden: true,
@@ -20,7 +21,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 		case CartActionTypes.ADD_ITEM:
 			return {
 				...state,
-				cartItems: [...state.cartItems, action.payload],
+				cartItems: addItemToCart(state.cartItems, action.payload),
 			};
 		default:
 			return state;
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+//========================================
+//  追加ボタン押すと、既存アイテムが保存されずに毎回クリックしたアイテムが配列に格納される
+//  accumulatorみたいなの必要
+//  すでに追加済みのアイテムをさらに追加しようとしているか？
+//========================================
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+	const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToAdd.id);
+
+	if (existingCartItem) {
+		return cartItems.map((cartItem) =>
+			cartItem.id === cartItemToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+		);
+	}
+	return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
