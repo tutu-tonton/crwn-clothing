82dd6711027977fa63ad4da0e4c45fbd4ded8584
tutu-tonton <tngt33844507@gmail.com>
2020-06-03 16:26:16 +0900

    implemented redux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,15 +6,30 @@
 //========================================
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-//  stateが出来上がる。 user, cartという名前で呼ぶ
-export default combineReducers({
+const persistConfig = {
+	key: 'root',
+	storage,
+	// 永続化したいreducerを書く
+	whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
 });
+
+//  stateが出来上がる。 user, cartという名前で呼ぶ
+// export default combineReducers({
+// 	user: userReducer,
+// 	cart: cartReducer,
+// });
+export default persistReducer(persistConfig, rootReducer);
 
 //  stateのcartに入っているもの. cartReducerを参照
 //  cart = {
