68040d09c4e8a4ba17abec11a47fabf2522fb6fc
tutu-tonton <tngt33844507@gmail.com>
2020-06-04 18:25:25 +0900

    命名変更: category -> collection

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import './category.styles.scss';
+import './collection.styles.scss';
 
 // shop.componentでRouteにラップされてる
-const CategoryPage = () => (
-	<div className="category">
-		<h2>CATEGORY PAGE</h2>
+const CollectionPage = () => (
+	<div className="collection">
+		<h2>COLLECTION PAGE</h2>
 	</div>
 );
 
-export default CategoryPage;
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -10,14 +10,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collection/collection.component';
 
 // app.js内でRouteされてるので、match使える
 const ShopPage = ({ match }) => (
 	// カテゴリ毎にCollectionPreviewコンポを出力
 	<div className="shop-page">
 		<Route exact path={`${match.path}`} component={CollectionsOverview} />
-		<Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+		<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
 	</div>
 );
 
