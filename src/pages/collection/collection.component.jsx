931302abe59167d4255344b542df704e2cabcd39
tutu-tonton <tngt33844507@gmail.com>
2020-06-04 20:01:47 +0900

    パスパラメータの文字列とidの数値を対応させるCOLLECTION_ID_MAP実装

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
 // shop.componentでRouteにラップされてる
-const CollectionPage = () => (
-	<div className="collection">
-		<h2>COLLECTION PAGE</h2>
-	</div>
-);
+const CollectionPage = ({ collection }) => {
+	console.log(collection);
+	return (
+		<div className="collection">
+			<h2>COLLECTION PAGE</h2>
+		</div>
+	);
+};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+	collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
