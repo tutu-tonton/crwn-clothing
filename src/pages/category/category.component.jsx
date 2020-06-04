f675a54d32ed14e940935460cbc80e4f4a636fb8
tutu-tonton <tngt33844507@gmail.com>
2020-06-04 18:07:15 +0900

    shopページから各カテゴリページへのRoute。途中まで。

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './category.styles.scss';
+
+// shop.componentでRouteにラップされてる
+const CategoryPage = () => (
+	<div className="category">
+		<h2>CATEGORY PAGE</h2>
+	</div>
+);
+
+export default CategoryPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,17 +3,21 @@
 //  各カテゴリ毎にタイトル・画像・商品名の組み合わせ <- 4つ分
 //  5カテゴリ分表示される
 //  stateにショップデータ保有. ショップデータは別ファイルで保管。インポートする
-//  > CollectionPreview.component
+//  削除> CollectionPreview.component
 //  > CollectionOverview
 //========================================
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CategoryPage from '../category/category.component';
 
-const ShopPage = ({ collections }) => (
+// app.js内でRouteされてるので、match使える
+const ShopPage = ({ match }) => (
 	// カテゴリ毎にCollectionPreviewコンポを出力
 	<div className="shop-page">
-		<CollectionsOverview />
+		<Route exact path={`${match.path}`} component={CollectionsOverview} />
+		<Route path={`${match.path}/:categoryId`} component={CategoryPage} />
 	</div>
 );
 
